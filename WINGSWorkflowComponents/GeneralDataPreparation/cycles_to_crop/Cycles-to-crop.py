0c36df09722aaf459f35705b1b75a4c27d6f56df
Daniel Garijo <dgarijo@isi.edu>
2019-01-18 14:06:56 -0800

    Cycles to crop component

--- WINGSWorkflowComponents/GeneralDataPreparation/cycles_to_crop/Cycles-to-crop.py
+++ WINGSWorkflowComponents/GeneralDataPreparation/cycles_to_crop/Cycles-to-crop.py
@@ -1,76 +1,82 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jul  6 14:12:29 2018
 
+"""
 @author: deborahkhider
+@author: rafaelfsilva
 
-Transformation code from CYCLES double output to Crop model
+Transformation code from CYCLES output to Crop model
 """
-#import numpy as np
-import tarfile
-import pandas as pd
+
+import configparser
 import csv
+import os
 import sys
+import numpy as np
+import pandas as pd
+from itertools import islice
 
-#
-#CYCLES needs to be run twice. One is the baseline scenario. The second has an
-#increase in 10% fertilizer. 
-#
-percent_fertilizer = 0.1 #This should become a parameter
+# select the year. In its present configuration only one year is allowed
+year = 2017
 
-# Set the tarfiles path
-base_run_file = sys.argv[2]
-scenario_run_file = sys.argv[1]
+data = {}
 
-# Open the base tar file
-base_run = tarfile.open(name = base_run_file)
-base_run_names = base_run.getnames()
-for item in base_run_names:
-    if 'season' in item:
-        file_name = item
-        base_file = base_run.extract(item)
+for f in sys.argv[1:-1]:
+    with open(f) as configfile:
+        config_string = ''.join([next(configfile) for x in range(3)])
+        config = configparser.ConfigParser()
+        config.read_string(config_string)
 
-# Open the files and look for the grain yield
-data = pd.read_table(file_name, delimiter = '\t',\
-                     skiprows = [1], index_col=False)
+        # read properties
+        crop_name = config.get('DEFAULT', 'crop-name')
+        percent_increase_fertilizer = float(config.get('DEFAULT', 'percent-increase-fertilizer'))/100
 
-# Get the Grain yield header
-for item in list(data):
-    if 'GRAIN YIELD' in item:
-        header = item
+        # basic properties
+        if crop_name not in data:
+            data[crop_name] = {}
+        if percent_increase_fertilizer != 0.0:
+            data[crop_name]['percent_increase_fertilizer'] = percent_increase_fertilizer
 
-base_grain = data[header].mean()
+    # Open the season config file and look for the grain yield
+    d = pd.read_table(f, delimiter = '\t', skiprows = [0,1,2,3,5], index_col=False)
 
-# Do the same with the increased fertilizer run
-scenario_run = tarfile.open(name = scenario_run_file)
-scenario_run_names = scenario_run.getnames()
-for item in scenario_run_names:
-    if 'season' in item:
-        file_name = item
-        scenario_file = scenario_run.extract(item)
+    # Make sure the dates are in datetime format
+    for item in list(d):
+        if 'DATE' in item:
+            header_time = item
+        if 'GRAIN YIELD' in item:
+            header = item
 
-# Open the files and look for the grain yield
-data = pd.read_table(file_name, delimiter = '\t',\
-                     skiprows = [1], index_col=False)
+    d[header_time] = pd.to_datetime(d[header_time])
+    time = d[header_time].dt.year.unique()
+    grain= float(d.groupby(d[header_time].dt.year)[header].transform('mean').unique())
+    data[crop_name]['base_grain' if percent_increase_fertilizer == 0.0 else 'scenario_grain'] = grain
 
-# Get the Grain yield header
-for item in list(data):
-    if 'GRAIN YIELD' in item:
-        header = item
+# Make sure the year is available
+if year in time:
+    index = np.where(time==year)[0][0]
+else:
+    sys.exit("Year not available in Cycles run")
 
-scenario_grain = data[header].mean()
-         
 # Calculate the yield %
-percent_yield = (scenario_grain-base_grain)/base_grain
-
-#elasticity
-el = percent_yield/percent_fertilizer
+for crop_name in data:
+    data[crop_name]['percent_yield'] = (data[crop_name]['scenario_grain'] - data[crop_name]['base_grain']) / data[crop_name]['base_grain']
+    #elasticity
+    data[crop_name]['elasticity'] = data[crop_name]['percent_yield'] / data[crop_name]['percent_increase_fertilizer']
 
 # write out as csv file
-csv_file = sys.argv[3]
+csv_file = sys.argv[-1:][0]
 
-with open(csv_file,'w',newline='') as csvfile:
+crops_econ = ['cassava','groundnuts','corn','sesame','sorghum']
+default_value = ['0.25','0.25','0.11','0.25','0.11']
+
+with open(csv_file, 'w', newline='') as csvfile:
     yieldwriter = csv.writer(csvfile, delimiter=',')
-    yieldwriter.writerow(['','yieldN'])
-    yieldwriter.writerow(['sorghum',el])
+    yieldwriter.writerow(['','ybarN'])
+    for idx, crop_econ in enumerate(crops_econ):
+        if crop_econ in (name.lower() for name in data):
+            tt = crop_econ
+            if crop_econ == "corn":
+                tt = "maize"
+            yieldwriter.writerow([tt, data[crop_econ.capitalize()]['elasticity']])
+        else:
+            yieldwriter.writerow([crop_econ, default_value[idx]])
