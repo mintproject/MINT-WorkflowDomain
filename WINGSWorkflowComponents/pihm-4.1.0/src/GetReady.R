65657df05b6761fc9c6e1a4b8a88de0a061a49dc
Daniel Garijo <dgarijo@isi.edu>
2019-10-31 21:26:51 -0700

    added pihm 4.1.0

---
+++ WINGSWorkflowComponents/pihm-4.1.0/src/GetReady.R
@@ -0,0 +1,87 @@
+#!/usr/bin/env Rscript
+# This file is used for pre-define the variables for the working environment.
+# Important vars:
+# 1. Path to Rawdata directory
+# 2. Path to FLDAS data directory
+# 3. Path to landuse data raster file
+# 4. Path to soil data directory
+# 5. Project name
+# 6. Years for simulation. startYear and endYear
+# 7. Output path.
+
+# Task:
+# 1. Set the path to rawdata, output, project name
+# 2. Load libraries.
+# 3. Create the folders.
+
+args = commandArgs(trailingOnly=TRUE)
+if (length(args)==0) {
+  fn.prj='project.txt'
+  # fn.prj='pongo.txt'
+}else{
+  print(args)
+  fn.prj = args[1]
+}
+message('Reading project file ', fn.prj)
+if(file.exists(fn.prj)){
+  tmp=as.matrix(read.table(fn.prj, header = F, row.names = 1))
+  cdir = tmp[,1]
+  print(cdir)
+}else{
+  stop('File missing: ', fn.prj)
+}
+
+dir.rawdata=cdir['dir.rawdata']
+dir.soil = cdir['dir.soil']
+dir.fldas = cdir['dir.fldas']
+dir.out = cdir['dir.out']
+
+prjname=cdir['prjname']
+years=as.numeric(cdir['startyear']): as.numeric(cdir['endyear'])
+
+fsp.wbd = cdir['fsp.wbd']
+fsp.stm = cdir['fsp.stm']
+
+fr.dem=cdir['fr.dem']
+fr.landuse = cdir['fr.landuse']
+NumCells = as.numeric(cdir['NumCells'])
+if(is.null(NumCells) | is.na(NumCells)){
+  NumCells = 1000;
+}
+AqDepth = as.numeric(cdir['AqDepth'])
+if(is.null(AqDepth) | is.na(AqDepth)){
+  AqDepth = 10;
+}
+# years=2017:2018
+dir.png =file.path(dir.out, 'Image')
+dir.pihmgis = file.path(dir.out, 'PIHMgis' )
+dir.pihmin <- file.path(dir.out, 'input', prjname)
+dir.pihmout <- file.path(dir.out, 'output', paste0(prjname, '.out') )
+dir.forc <- file.path(dir.out, 'forcing')
+
+tmp=lapply(list(dir.out,dir.png, dir.pihmgis, dir.pihmin, dir.pihmout, dir.forc), dir.create, showWarnings=F, recursive=T)
+
+
+library(raster)
+library(sp)
+library(rgeos)
+library(rgdal)
+library(PIHMgisR)
+library(lattice)
+library(ggplot2)
+library(GGally)
+library(hydroTSM)
+library(hydroGOF)
+
+crs.gcs = sp::CRS('+init=epsg:4326')
+
+# Some Constant values in the working environments.
+dist.buffer = 2000 #distance to build the buffer region.
+
+ext.fldas = c(22, 51.4, -11.8, 23.0) # Range of FLDAS East Africa.
+# 0.1 deg resolution in FLDAS
+# 0.25 deg resolution in GLDAS
+res=as.numeric(cdir['weather.res'])
+if(is.null(res) | is.na(res)){
+  res = 0.1;
+}
